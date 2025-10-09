d7a4d8b15d86ca558baf2ef5028182fcf6f1832c
Oleksii Berezhok <berolan88@gmail.com>
2025-10-09 13:49:25 +0300

    add lesson_3_1 home work

--- lesson_3_1.py
+++ lesson_3_1.py
@@ -1,11 +1,18 @@
 a = int(input("Введіть перше число:"))
 b = int(input("Введіть друге число:"))
-c = input("Введіть математичну дію (+, - , *, /):")
-if c == "+" :
-    print (a+b)
-elif c == "-":
-    print(a-b)
-elif c == "*":
-    print(a*b)
+if b != 0:
+    c = input("Введіть математичну дію (+, - , *, /):")
+    if c == "+" :
+        print (a+b)
+    elif c == "-":
+        print(a-b)
+    elif c == "*":
+        print(a*b)
+    elif c == "/":
+        print(a / b)
+    elif c == 0 :
+        print("Не можна ")
+    else:
+        print("Ви ввели не вірну дію")
 else:
-    print(a/b)
+    print("На ноль не можна ділити")
